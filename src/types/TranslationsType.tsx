5c9c29a1de6ff6d2a5a80ab97caae62768484282
awallace77 <andrew77wallace@outlook.com>
2026-01-20 20:03:31 -0500

    Removed python requirements.txt, update readme

--- src/types/TranslationsType.tsx
+++
@@ -1,9 +0,0 @@
-export type TranslationsType = {
-  general: { [key: string]: string };
-  actions: { [key: string]: string };
-  errors: { [key: string]: string };
-  success: { [key: string]: string };
-  warning: { [key: string]: string };
-  info: { [key: string]: string };
-  generatorTypes: { [key: string]: string };
-};
--- src/utils/utils.tsx
+++
@@ -1,9 +0,0 @@
-export const getCurrentLocale: () => "en" | "fr" = () => {
-  let lang;
-  if (navigator.languages && navigator.languages.length) {
-    lang = navigator.languages[0];
-  } else {
-    lang = navigator.language;
-  }
-  return lang && lang.includes("fr") ? "fr" : "en";
-};
